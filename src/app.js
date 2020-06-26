ebf71129ec84c8d51c3bc2c930c47f061fba648a
Shyaboi <ian.shane.sears@gmail.com>
2020-06-25 23:02:55 -0700

    added 1st image with webpack

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
-import Home from '../index.html'
-import Styles from './scss/app.scss'
-import 'bootstrap'
+import Home from "../index.html";
+import Styles from "./scss/app.scss";
+import "bootstrap";
+// importing popper since it was already in the package.json
+import "popper.js";
+// importing jquery since it was already in the package.json
+import "jquery";
 
-console.log("working")
+// importing hero photto
+import muhHero from '../src/assets/hero.jpg'
+
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    
+const Hero = new Image();
+Hero.src = muhHero
+element.appendChild(Hero);
+
+return element;
+}
+
+Hero()
+
+console.log("working");
