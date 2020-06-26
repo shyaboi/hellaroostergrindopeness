d476b1da6d4e0090c5b9c60e732f985bdded5222
Shyaboi <ian.shane.sears@gmail.com>
2020-06-25 23:08:38 -0700

    fix bork

--- src/app.js
+++ src/app.js
@@ -6,24 +6,5 @@ import "popper.js";
 // importing jquery since it was already in the package.json
 import "jquery";
 
-// importing hero photto
-import muhHero from '../src/assets/hero.jpg'
-
-function component() {
-    const element = document.createElement('div');
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    
-const Hero = new Image();
-Hero.src = muhHero
-element.appendChild(Hero);
-
-return element;
-}
-
-Hero()
 
 console.log("working");
