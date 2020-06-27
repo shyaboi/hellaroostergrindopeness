0474f7b36d4bd6c3f7eedc7ed75a68b3177fbf85
Shyaboi <ian.shane.sears@gmail.com>
2020-06-27 10:15:02 -0700

    bundle change dir

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import Home from "../index.html";
 import Styles from "./scss/app.scss";
+// autoprefixer
+// import "autoprefixer";
+
 import "bootstrap";
 // importing popper since it was already in the package.json
 import "popper.js";
@@ -9,4 +12,4 @@ import "jquery";
 
 
 
-<Home />
+// render(<Home />)
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/app.js",
 
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "./"),
     filename: "bundle.js",
   },
   watch: true,
