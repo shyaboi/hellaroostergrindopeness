7d33c7f580479d806379fc1c0eb7980c1d9f7e40
Shyaboi <ian.shane.sears@gmail.com>
2020-06-28 21:01:20 -0700

    all files compiling into dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,33 @@
 const path = require("path");
 const webpack = require("webpack");
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const extractPlugin = new ExtractTextPlugin ({
+  filename:'main.css'
+})
 
 module.exports = {
-  entry: [ "./src/app.js",
-  ],
+  entry: "./src/app.js",
+  
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath:"/dist"
+    // publicPath:"/dist"
   },
   watch: true,
   devServer: {
     inline: true,
   },
+  
+  
   module: {
     rules: [
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use:['css-loader', 'sass-loader']
+        })
+      },
       {
         test: /\.html$/,
         loader: "html-loader",
@@ -28,31 +41,14 @@ module.exports = {
           publicPath:'assets/'
         },
       },
+    ]
       //Add sass-loader
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-          },
-          {
-            loader: "sass-loader",
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-			  plugins:
-			  
-			  function () {
-                return [require("autoprefixer")];
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-};
+      
+    },
+    plugins: [
+      extractPlugin,
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      })
+    ]
+  }
